f0e62f9d6fe2d70d4a2f2dfdb7bf1a8a5d0b425a
luan11 <luaannovais@live.com>
2019-06-18 16:03:26 -0300

    estilo do pdf finalizado

--- index.php
+++ index.php
@@ -1,53 +1,91 @@
 <?php
 require_once('dompdf/autoload.inc.php');
+// define('DOMPDF_ENABLE_CSS_FLOAT ', true);
 
 $domPdf = new \Dompdf\Dompdf();
 $domPdf->loadHtml('
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>VIP Card</title>
-    <style>
-        body { font-family: sans-serif; color: #e9e9e9; background-color: #fff; }
-        .vip-card-pdf-header-infos, .vip-card-pdf-order-infos{
-            background-color: #3d3d3d;
-            padding: 10px;
-            border-radius: 12px;
-            border: 1px solid #2a2a2a;
-        }
-        .vip-card-pdf-header-infos h2,  .vip-card-pdf-order-infos h3,.vip-card-pdf-order-infos p>b{
-            color: #ffd560;
-        }
-        .vip-card-pdf-logo{
-            width: 150px;
-            height: 151px;
-            display: block;
-            margin: 10px auto;
-            background-image: url("logo.png");
-            background-size: 150px;
-        }
-    </style>
-</head>
+<style>
+    body { font-family: sans-serif; color: #4d4d4d; background-color: #fff; }
+    .card-pdf-logo{
+        width: 115px;
+        height: 115px;
+        display: block;
+        margin: 20px auto;
+        background-image: url("logo-for-pdf.png");
+    }
+    p{
+        margin: 0 0 5px;
+        font-size: 16px;
+    }
+    ul p{
+        text-align: left;
+        margin-bottom: 8px;
+    }
+    .title{
+        margin: 0 0 10px;
+        font-size: 18px;
+        text-align: center;
+        text-transform: uppercase;
+    }
+    .card-pdf-info-container{
+        text-align: center;
+        padding: 8px 8px 5px;
+        margin: 10px 0;
+        border-top: 1px solid #505050;
+    }
+    .card-pdf-image{
+        width: 650px;
+        height: 413px;
+        display: block;
+        margin: auto;
+        background-image: url("card-prime-image.jpg");
+    }
+    .vip-card-code{
+        width: 100%;
+        display: block;
+        text-align: center;
+        padding: 10px 0;
+        font-size: 20px;
+        color: #FFD560;
+        background-color: #2A2A2A;
+        font-weight: bold;
+    }
+    .card-pdf-order-total{ text-align: right; }
+</style>
 <body>
-    <div class="vip-card-pdf-logo"></div>
-    <div class="vip-card-pdf-header-infos">
-        <h2>Título do VIP Card (Carga horária) - Quantidade de Pessoas</h2>
-        <p><b>Data de emissão:</b> 00/00/0000</p>
-        <p><b>Validade:</b> 00/00/0000</p>
+    <div class="card-pdf-logo"></div>
+    <div class="card-pdf-info-container">
+        <h1 class="title">Urban Card Prime</h1>
+        <p><b>Emitido em:</b> 18/06/2019</p>
+        <p><b>Valido até:</b> 18/09/2019</p>        
+        <p><b>Nome:</b> Luan Novais</p>
+        <p><b>CPF:</b> 000.000.000-00</p>
+        <p><b>Passaporte:</b> 000000000</p>
+    </div>    
+    <div class="card-pdf-image"></div>
+    <div class="card-pdf-info-container">
+        <h2 class="title">CÓDIGO DO CARD</h2>
+        <p class="vip-card-code">1ED49BC4-1973-45E4-9C86-DF6E037C7AAC</p>
     </div>
-    <div class="vip-card-pdf-order-infos">
-        <h3>Resumo do pedido:</h3>
+    <div style="page-break-before: always;"></div>
+    <div class="card-pdf-logo"></div>
+    <div class="card-pdf-info-container">
+        <h2 class="title">Resumo do pedido</h2>
         <ul>
-            <li>Título do VIP Card (Carga horária) Quantidade de Pessoas: <b>R$ 170,00</b></li>
-            <li>Adicional 1: <b>R$ 30,00</b></li>
+            <li>
+                <p><b>Urban Card Prime (3h)</b> - R$ 95,00</p>
+            </li>
+            <li>
+                <p><b>Concierge no EMBARQUE</b> - R$ 30,00</p>
+            </li>
+            <li>
+                <p><b>Concierge no DESEMBARQUE</b> - R$ 30,00</p>
+            </li>
+            <hr>
+            <p class="card-pdf-order-total"><b>TOTAL:</b> R$ 155,00</p>
         </ul>
-        <p><b>TOTAL:</b> R$ 200,00</p>
     </div>
 </body>
-</html> 
 ');
 $domPdf->setPaper('A4');
 $domPdf->render();
